a1d5b26fbf78136f72aa0fb50ce7a9e335043d36
Adish474 <82947647+Adish474@users.noreply.github.com>
2026-02-08 02:00:37 +0530

    Create train.py

---
+++ train.py
@@ -0,0 +1,5 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from model import CustomLogisticRegression
